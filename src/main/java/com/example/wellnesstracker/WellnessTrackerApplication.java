2d6c0fabb710edcc91b1c7a2ebea1e29f5bc89ec
quangptt0910 <quangptt0910@gmail.com>
2025-06-24 23:52:21 +0200

    Update backend

--- src/main/java/com/example/wellnesstracker/WellnessTrackerApplication.java
+++ src/main/java/com/example/wellnesstracker/WellnessTrackerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.time.LocalDate;
 
